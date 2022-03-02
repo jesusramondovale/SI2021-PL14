fddc3dc9bbb8b177de14b5f290c26108c7295e89
UO261060 <UO261060@DESKTOP-A9MB9D4.mshome.net>
2022-03-02 12:46:16 +0100

    DTO + View - v1

---
+++ samples-test-dev/src/main/java/giis/demo/Proyecto/View/MostrarActividadesView.java
@@ -0,0 +1,181 @@
+package giis.demo.Proyecto.View;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class MostrarActividadesView {
+
+	private JFrame frame;
+	private JTable table;
+	
+	
+	private JButton btnLimpiar;
+	private JButton btnBuscar;
+	private JLabel lblPeriodo;
+	private JComboBox comboBoxPeriodo;
+	private JScrollPane scrollPane; 
+	
+	
+
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	public void setFrame(JFrame frame) {
+		this.frame = frame;
+	}
+
+	public JTable getTable() {
+		return table;
+	}
+
+	public void setTable(JTable table) {
+		this.table = table;
+	}
+
+	public JButton getBtnLimpiar() {
+		return btnLimpiar;
+	}
+
+	public void setBtnLimpiar(JButton btnLimpiar) {
+		this.btnLimpiar = btnLimpiar;
+	}
+
+	public JButton getBtnBuscar() {
+		return btnBuscar;
+	}
+
+	public void setBtnBuscar(JButton btnBuscar) {
+		this.btnBuscar = btnBuscar;
+	}
+
+	public JLabel getLblPeriodo() {
+		return lblPeriodo;
+	}
+
+	public void setLblPeriodo(JLabel lblPeriodo) {
+		this.lblPeriodo = lblPeriodo;
+	}
+
+	public JComboBox getComboBoxPeriodo() {
+		return comboBoxPeriodo;
+	}
+
+	public void setComboBoxPeriodo(JComboBox comboBoxPeriodo) {
+		this.comboBoxPeriodo = comboBoxPeriodo;
+	}
+
+	public JScrollPane getScrollPane() {
+		return scrollPane;
+	}
+
+	public void setScrollPane(JScrollPane scrollPane) {
+		this.scrollPane = scrollPane;
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MostrarActividadesView window = new MostrarActividadesView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MostrarActividadesView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Actividades");
+		frame.setName("Actividades");
+		frame.setBounds(100, 100, 450, 300);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnLimpiar = new JButton("Limpiar");
+		
+		JLabel lblPeriodo = new JLabel("Seleccionar Periodo");
+		lblPeriodo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JComboBox comboBoxPeriodo = new JComboBox();
+		comboBoxPeriodo.setModel(new DefaultComboBoxModel(new String[] {"Enero", "Junio", "Septiembre"}));
+		comboBoxPeriodo.setToolTipText("");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addGap(27)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+									.addComponent(comboBoxPeriodo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED, 153, Short.MAX_VALUE)
+									.addComponent(btnLimpiar, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+									.addGap(18)
+									.addComponent(btnBuscar))
+								.addComponent(lblPeriodo)))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 185, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblPeriodo)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnBuscar)
+						.addComponent(btnLimpiar)
+						.addComponent(comboBoxPeriodo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
+		
+		table = new JTable();
+		scrollPane.setColumnHeaderView(table);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+	
+	
+	
+	
+	
+}
