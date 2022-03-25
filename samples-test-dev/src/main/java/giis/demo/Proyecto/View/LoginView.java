12707ee12ab3c1a197005f1f014b2f3d30a10345
Jesús Ramón <uo261060@uniovi.es>
2022-03-25 22:03:48 +0100

    [master] - Añadido Pantalla Login Usuario ** Necesario añadir más adelante esa vista a los controladores que haga falta.

---
+++ samples-test-dev/src/main/java/giis/demo/Proyecto/View/LoginView.java
@@ -0,0 +1,112 @@
+package giis.demo.Proyecto.View;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class LoginView {
+
+	private JFrame frame;
+	private JTextField txtId;
+	private JButton btnLogear;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginView window = new LoginView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 484, 222);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		btnLogear = new JButton("LOGIN");
+		btnLogear.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		
+		JLabel lblNewLabel = new JLabel("INTRODUZCA SU IDENTIFICADOR");
+		
+		txtId = new JTextField();
+		txtId.setColumns(10);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(340, Short.MAX_VALUE)
+					.addComponent(btnLogear)
+					.addGap(39))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(txtId, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(187, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(65, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(txtId, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(53)
+					.addComponent(btnLogear)
+					.addGap(20))
+		);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	public void setFrame(JFrame frame) {
+		this.frame = frame;
+	}
+
+	public JTextField getTxtId() {
+		return txtId;
+	}
+
+	public void setTxtId(JTextField txtId) {
+		this.txtId = txtId;
+	}
+
+	public JButton getBtnLogear() {
+		return btnLogear;
+	}
+
+	public void setBtnLogear(JButton btnLogear) {
+		this.btnLogear = btnLogear;
+	}
+	
+	
+
+}
