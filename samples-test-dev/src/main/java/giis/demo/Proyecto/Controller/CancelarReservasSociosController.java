dff5be9f85d6d559e7833c49d737f38de7d340d3
UO269603 <UO269603@DESKTOP-BR3JF66>
2022-03-28 17:12:22 +0200

    Cambios en el model

--- samples-test-dev/src/main/java/giis/demo/Proyecto/Controller/CancelarReservasSociosController.java
+++ samples-test-dev/src/main/java/giis/demo/Proyecto/Controller/CancelarReservasSociosController.java
@@ -1,26 +1,67 @@
 package giis.demo.Proyecto.Controller;
 
 import giis.demo.Proyecto.Model.CancelarReservasSociosModel;
-import giis.demo.Proyecto.Model.visualizarReservasInstalacionesModel;
 import giis.demo.Proyecto.View.CancelarReservasSociosView;
-import giis.demo.Proyecto.View.visualizaReservasInstalacionesView;
+import giis.demo.Proyecto.View.LoginView;
+import giis.demo.util.SwingUtil;
 
 public class CancelarReservasSociosController {
 
 	private CancelarReservasSociosModel model;
 	private CancelarReservasSociosView view;
-	private int pos;
+	private LoginView login;
 
 	public CancelarReservasSociosController(CancelarReservasSociosModel model, CancelarReservasSociosView view) {
+		this.login = login;
 		this.model = model;
 		this.view = view;
 		this.initView();
 	}
 
 	public void initView() {
-		view.getframe().setVisible(true);
+		view.getframe().setVisible(false); 
+		this.login.getFrame().setVisible(true);
 	}
-	
-	
+
+	public void initController() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	// Código que se ejecuta al pulsar en el botón Login en LoginView
+
+	private void login() {
+
+		//Extrae el Socio introducido en la vista del login
+		String idSocio = this.login.getTxtId().getText();
+
+		try {
+			//Delega en el model para saber si ese socio existe
+			if (model.isSocio(Integer.parseInt(idSocio))) {
+
+				//Si el socio existe, hace invisible la pantalla de login y hace visible la pantalla de estado de contabilidad
+				this.idSocio = idSocio;
+				this.login.getFrame().setVisible(false);
+				this.view.getframe().setVisible(true);
+				SwingUtil.showMessage("Bienvenido! Socio #" + idSocio, "Saludos de nuevo", 2);
+
+			}
+			else {
+
+				//Si no existe el socio ...
+				SwingUtil.showMessage("El Socio #" + idSocio + " no existe en la BD!", "Vaya...", 0);
+
+			}
+		}
+		catch (NumberFormatException e){
+
+			SwingUtil.showMessage("Error! Introduzca un #ID de Socio válido (numérico)", "Vaya...", 0);
+
+
+		}
+
+
+	}
 
 }
