0d741d3ecaf15bea47c5730cb13db88bec57d4f6
UO269603 <UO269603@DESKTOP-BR3JF66>
2022-03-28 17:14:27 +0200

    Cambios Controller y model

--- samples-test-dev/src/main/java/giis/demo/Proyecto/Model/CancelarReservasSociosModel.java
+++ samples-test-dev/src/main/java/giis/demo/Proyecto/Model/CancelarReservasSociosModel.java
@@ -1,17 +1,44 @@
 package giis.demo.Proyecto.Model;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import giis.demo.Proyecto.DTO.SociosDTO;
 import giis.demo.Proyecto.DTO.SociosDisplayDTO;
 import giis.demo.Proyecto.DTO.actividadReservaInstalacionDTO;
 import giis.demo.Proyecto.DTO.estadoReservaDTO;
 import giis.demo.util.Database;
+import giis.demo.util.SwingUtil;
+import giis.demo.util.UnexpectedException;
 
 public class CancelarReservasSociosModel {
 
 	private Database db = new Database();
 
+	private static String SQL3 = "SELECT idSocio , nombre "
+			+ "FROM socios "
+			+ "WHERE idSocio = ? ";
+	
+	// Función para comprobar si el ID de socio existe
+	// Returns true si existe, false si no
+	public boolean isSocio(int idSocio) {
+
+		List<SociosDTO> socios = new ArrayList<SociosDTO>();
+
+		try {
+			socios = db.executeQueryPojo(SociosDTO.class, SQL3, idSocio);
+		}
+		catch(UnexpectedException e) {
+
+			SwingUtil.showMessage("Error SQL -> EstadoCuentasModel", "Error", 0);
+
+		}
+
+		return (!socios.isEmpty());
+
+
+	}
 
 	public List<Object[]> getListaInstalaciones(){
 
