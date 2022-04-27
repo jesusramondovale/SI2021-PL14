4b3be30c371a27980a47ec8eae603d2737d3d292
UO261060 <UO261060@DESKTOP-A9MB9D4.mshome.net>
2022-04-27 11:30:13 +0200

    [17748] - View Completado

--- samples-test-dev/src/main/java/giis/demo/Proyecto/View/InscribirClientesView.java
+++ samples-test-dev/src/main/java/giis/demo/Proyecto/View/InscribirClientesView.java
@@ -3,10 +3,22 @@ package giis.demo.Proyecto.View;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class InscribirClientesView {
 
-	private JFrame frame;
+	private JFrame frmInscribirClientesEn;
+	private JTextField tfIDCliente;
+	private JTable tableActividades;
+	private JScrollPane scrollPaneActividades;
+	private JButton btnRefresh;
+	private JButton btnInscribir;
+	private JButton btnLimpiar;
 
 	/**
 	 * Launch the application.
@@ -16,7 +28,7 @@ public class InscribirClientesView {
 			public void run() {
 				try {
 					InscribirClientesView window = new InscribirClientesView();
-					window.frame.setVisible(true);
+					window.frmInscribirClientesEn.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -31,13 +43,111 @@ public class InscribirClientesView {
 		initialize();
 	}
 
+	
+	
+	
+	public JFrame getFrmInscribirClientesEn() {
+		return frmInscribirClientesEn;
+	}
+
+	public void setFrmInscribirClientesEn(JFrame frmInscribirClientesEn) {
+		this.frmInscribirClientesEn = frmInscribirClientesEn;
+	}
+
+	public JTextField getTfIDCliente() {
+		return tfIDCliente;
+	}
+
+	public void setTfIDCliente(JTextField tfIDCliente) {
+		this.tfIDCliente = tfIDCliente;
+	}
+
+	public JTable getTableActividades() {
+		return tableActividades;
+	}
+
+	public void setTableActividades(JTable tableActividades) {
+		this.tableActividades = tableActividades;
+	}
+
+	public JScrollPane getScrollPaneActividades() {
+		return scrollPaneActividades;
+	}
+
+	public void setScrollPaneActividades(JScrollPane scrollPaneActividades) {
+		this.scrollPaneActividades = scrollPaneActividades;
+	}
+
+	public JButton getBtnRefresh() {
+		return btnRefresh;
+	}
+
+	public void setBtnRefresh(JButton btnRefresh) {
+		this.btnRefresh = btnRefresh;
+	}
+	
+	
+
+	public JButton getBtnInscribir() {
+		return btnInscribir;
+	}
+
+	public void setBtnInscribir(JButton btnInscribir) {
+		this.btnInscribir = btnInscribir;
+	}
+
+	public JButton getBtnLimpiar() {
+		return btnLimpiar;
+	}
+
+	public void setBtnLimpiar(JButton btnLimpiar) {
+		this.btnLimpiar = btnLimpiar;
+	}
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmInscribirClientesEn = new JFrame();
+		frmInscribirClientesEn.setTitle("Inscribir Clientes en Actividades");
+		frmInscribirClientesEn.setBounds(100, 100, 324, 352);
+		frmInscribirClientesEn.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("#ID del Cliente");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(21, 21, 118, 22);
+		frmInscribirClientesEn.getContentPane().add(lblNewLabel);
+		
+		tfIDCliente = new JTextField();
+		tfIDCliente.setBounds(148, 21, 110, 20);
+		frmInscribirClientesEn.getContentPane().add(tfIDCliente);
+		tfIDCliente.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Actividades Disponibles");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1.setBounds(18, 71, 177, 22);
+		frmInscribirClientesEn.getContentPane().add(lblNewLabel_1);
+		
+		btnRefresh = new JButton("Refresh");
+		btnRefresh.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnRefresh.setBounds(205, 70, 89, 23);
+		frmInscribirClientesEn.getContentPane().add(btnRefresh);
+		
+		scrollPaneActividades = new JScrollPane();
+		scrollPaneActividades.setBounds(21, 102, 273, 163);
+		frmInscribirClientesEn.getContentPane().add(scrollPaneActividades);
+		
+		tableActividades = new JTable();
+		scrollPaneActividades.setViewportView(tableActividades);
+		
+		btnInscribir = new JButton("Inscribir");
+		btnInscribir.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnInscribir.setBounds(205, 276, 89, 23);
+		frmInscribirClientesEn.getContentPane().add(btnInscribir);
+		
+		btnLimpiar = new JButton("Limpiar");
+		btnLimpiar.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnLimpiar.setBounds(21, 277, 89, 23);
+		frmInscribirClientesEn.getContentPane().add(btnLimpiar);
 	}
-
 }
